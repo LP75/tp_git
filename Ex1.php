9d6926bb25f03266f2c081a26e183f3bfecc30fd
Loïc Plantive <lo.pla231@gmail.com>
2024-02-12 18:44:11 +0100

    Ajout de texte à Ex1.php

--- Ex1.php
+++ Ex1.php
@@ -8,7 +8,8 @@
    <h1>Exercice 1FZ2dz22</h1>
    <?php
       $date = date('Y-m-d');
-      echo "La date d'aujourd'hui est : $date";
+      echo "<div>La date d'aujourd'hui est : $date</div>";
    ?>
+   <p>Et je teste git et VSCode </p>
 </body>
 </html>
